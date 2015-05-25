35a9949433a7415318b2c98951e406db119c2654
NoobLevler <nooblevler@localhost.localdomain>
2015-05-25 11:46:28 +0000

    forgot half the files

--- Client/Client.c
+++ Client/Client.c
@@ -1,8 +1,79 @@
-#include <iostream>
+#include <string.h>
+#include "Client.h"
 
-using namespace std;
+int main()
+{
+    /**/
+    int userName  = 123;
+    char buff[32];
+    int registerPipe, inputPipe, outputPipe;/*Input and Output Refer to MissionControl*/
+    int answer;
 
-int main() {
-    cout << "Hello, World!" << endl;
+    /*Get User Credentials*/
+
+    printf("Welcome, Please Enter Your Username (31 Char Limit): ");
+    //scanf("%d", &userName);
+    printf("\n");
+
+    /*END*/
+
+    /*Warn the Server of Client*/
+    sprintf(buff, "%d", userName);
+    registerPipe = open("/tmp/missionControlInput.pipe", O_WRONLY);
+    write(registerPipe, buff, 4);
+
+    /*Get the pipes to the server*/
+    sprintf(buff, "/tmp/%04xInput.pipe", userName);
+    do{
+        inputPipe = open( buff , O_WRONLY);
+    }
+    while(inputPipe <= 0);/*Hold Until the Server Creates the Pipe*/
+    sprintf(buff, "/tmp/%04xOutput.pipe", userName);
+    do{
+        outputPipe = open( buff , O_RDONLY);
+    }
+    while(outputPipe <= 0);/*Hold Until the Server Creates the Pipe*/
+
+    printf("PIPED");
+    //write(inputPipe, "\0", 1);
+    printf("What do you wish to do?\n1 - Input Commands\n2 - Check Balance\n3 - Update Balance\n");
+    scanf("%d", &answer);
+
+    switch (answer)
+    {
+        case 1:
+            commandDialog(inputPipe, outputPipe, userName);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            return -1;
+    }
+    read(outputPipe, buff, sizeof buff);
+    printf(buff);
+    printf("\n");
     return 0;
+}
+
+
+void commandDialog(int inputPipe, int outputPipe, int userName)
+{
+    char buff[64];
+    char stB[64];
+    printf("You are now connected to the server, Ctlr+D disconnects\n");
+    Intent i;
+    i.msgId = MSG_EXEC_CMD;
+
+    memcpy(stB, &i, sizeof i);
+    write(inputPipe, stB, sizeof i);
+
+    while(1)
+    {
+        memset(buff,0, 64);
+        scanf("%s", buff);
+        write(inputPipe, buff, strlen(buff));
+
+    }
 }
--- Client/Client.h
+++ Client/Client.h
@@ -5,4 +5,25 @@
 #ifndef CLIENT_CLIENT_H
 #define CLIENT_CLIENT_H
 
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include "stdio.h"
+#include "stdlib.h"
+
+#define MSG_EXEC_CMD 0
+#define MSG_BAL_CHECK 1
+#define MSG_BAL_UPDATE 2
+
+typedef struct intent{
+    int msgId, dataSize;
+}Intent;
+
+int main();
+void commandDialog(int inputPipe, int outputPipe, int userName);
+
 #endif //CLIENT_CLIENT_H
